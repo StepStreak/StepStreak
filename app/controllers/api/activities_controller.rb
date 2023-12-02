bcbcfce5009646d13d8fa6f74b50b7f3bbefb87e
Mohamad <mrahmoun93@gmail.com>
2023-12-02 16:14:40 +0100

    handle empty activity array

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,8 +1,9 @@
 module Api
   class ActivitiesController < ApplicationController
+    before_action :check_params, only: :create
     def create
       ActivitiesJob.perform_later(activity_params)
-      render json: { message: 'Activities created' }, status: :accepted
+      render json: { message: 'Activities are being processed' }, status: :accepted
     end
 
     private
@@ -17,6 +18,17 @@ module Api
           :calories,
           :resting_heart_rate
         )
+      end
+    end
+
+    def check_params
+      if params[:activity].blank?
+        Turbo::StreamsChannel.broadcast_update_to(
+          "activities_turbo_native",
+          target: 'dashboard-details',
+          content: '<turbo-frame id="dashboard" src="http://192.168.0.89:3000/dashboard" data-controller="reload">' )
+
+        render json: { message: 'Activity params are missing' }, status: :unprocessable_entity
       end
     end
   end
