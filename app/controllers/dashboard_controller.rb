bfa06234404ea2344cff3d58601023cb0d318807
Mohamad <mrahmoun93@gmail.com>
2023-12-10 16:58:33 +0100

    set encoded token

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  before_action :encoded_token, only: :show
+  before_action :set_encoded_token, only: :show
 
   def index
   end
@@ -10,7 +10,9 @@ class DashboardController < ApplicationController
 
   private
 
-  def encoded_token
-    @encoded_token ||= JWT.encode({ user_id: current_user.id}, Rails.application.credentials.secret_key_base, 'HS256')
+  def set_encoded_token
+    @encoded_token = JWT.encode({ user_id: current_user.id},
+                                Rails.application.credentials.secret_key_base,
+                                'HS256')
   end
 end
