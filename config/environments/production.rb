189cde2b3a973e6ee21e30176bbbd2d013e518de
Mohamad <mrahmoun93@gmail.com>
2023-12-11 22:21:27 +0100

    disable_request_forgery_protection on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.disable_request_forgery_protection = true
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
