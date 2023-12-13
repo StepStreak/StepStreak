13150dc3ac964ef54d051940a817878f761c47a6
Mohamad <mrahmoun93@gmail.com>
2023-12-13 21:12:10 +0100

    update score when user joins the challenge

--- app/models/challenge_user.rb
+++ app/models/challenge_user.rb
@@ -3,4 +3,12 @@ class ChallengeUser < ApplicationRecord
   belongs_to :challenge
 
   validates :user_id, uniqueness: { scope: :challenge_id }
+
+  after_create :update_score
+
+  def update_score
+    score = self.user.activities.where(date: self.challenge.starts_at.to_date..Date.current).sum(:steps)
+
+    self.update(score: score)
+  end
 end
