9374a41d49056aea385be97bee7ae73935bc1f13
Mohamad <mrahmoun93@gmail.com>
2023-12-16 20:15:55 +0100

    fix locked challenges validations

--- app/models/challenge_user.rb
+++ app/models/challenge_user.rb
@@ -4,10 +4,12 @@ class ChallengeUser < ApplicationRecord
   belongs_to :user
   belongs_to :challenge
 
-  validates :user_id, uniqueness: { scope: :challenge_id }
-  validate :code_validity, if: -> { challenge.locked? }
+  with_options on: :create do
+    validates :user_id, uniqueness: { scope: :challenge_id }
+    validate :code_validity, if: -> { challenge.locked? }
+  end
 
-  after_create :update_score
+  after_create_commit :update_score
 
   def update_score
     score = self.user.activities.where(date: self.challenge.starts_at.to_date..Date.current).sum(:steps)
