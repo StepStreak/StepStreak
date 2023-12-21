bce9e85526d5dd1c68eb555d3095db3f555dec10
Mohamad <mrahmoun93@gmail.com>
2023-12-22 00:58:21 +0100

    print request info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :app_version, :device_type
 
   before_action :authenticate_user!, unless: -> { request.format.json? }
+  before_action :prints_request_info
 
   def app_version
     request.user_agent.to_s.split('-').last.to_f
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
 
   def device_type
     request.user_agent.to_s.match?(/iOS/) ? 'ios' : 'android'
+  end
+
+  def prints_request_info
+    Rails.logger.info app_version
+    Rails.logger.info device_type
   end
 end
