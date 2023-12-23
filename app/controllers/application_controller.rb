1f514e68a8e3dfac8d1b4cd21a5384e9c1365c7e
Mohamad <mrahmoun93@gmail.com>
2023-12-23 18:31:15 +0100

    get more analytics information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!, unless: -> { request.format.json? }
   before_action :prints_request_info, unless: -> { request.format.json? }
+  before_action :track, unless: -> { request.format.json? }
 
   def app_version
     if device_type == 'ios'
@@ -31,5 +32,13 @@ class ApplicationController < ActionController::Base
   def prints_request_info
     Rails.logger.info app_version
     Rails.logger.info device_type
+  end
+
+  def track
+    if current_user
+      Logtail.with_context(user: { id: current_user.id }) do
+        Rails.logger.info("Request: #{request.method} #{request.path}")
+      end
+    end
   end
 end
