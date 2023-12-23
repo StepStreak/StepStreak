a027c639b2951c7d4f1cf8a901392b4142a747d7
Mohamad <mrahmoun93@gmail.com>
2023-12-23 18:38:53 +0100

    add /up endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'up' => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root "dashboard#index"
