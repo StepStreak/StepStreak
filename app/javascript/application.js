d6b9b564de636de0f4a034de89dcae46dd8fff5b
Mohamad <mrahmoun93@gmail.com>
2024-01-24 02:40:36 +0100

    few improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "flowbite/dist/flowbite.turbo.js";
 import "chartkick/chart.js"
