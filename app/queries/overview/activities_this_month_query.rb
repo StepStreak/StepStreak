63c55bef34e1f5a14f2bc15ff573aafe5dd9e6e3
Mohamad <mrahmoun93@gmail.com>
2024-01-28 00:42:18 +0100

    use end_of_month in ActivitiesThisMonthQuery

--- app/queries/overview/activities_this_month_query.rb
+++ app/queries/overview/activities_this_month_query.rb
@@ -1,7 +1,7 @@
 module Overview
   class ActivitiesThisMonthQuery
     def self.call
-      ActivitiesInPeriodQuery.call(Date.current.beginning_of_month, Date.current)
+      ActivitiesInPeriodQuery.call(Date.current.beginning_of_month, Date.current.end_of_month)
     end
   end
 end
