abe4dfb49df8349ca6130122ac8cc99d61664f6c
Mohamad <mrahmoun93@gmail.com>
2024-02-02 18:40:24 +0100

    change challenge card participants label

--- app/helpers/challenge_helper.rb
+++ app/helpers/challenge_helper.rb
@@ -12,6 +12,14 @@ module ChallengeHelper
     end
   end
 
+  def challenge_participants_count(challenge)
+    challenge.team? ? challenge.teams.count : challenge.challenge_users.count
+  end
+
+  def challenge_participants_label(challenge)
+    challenge.team? ? 'Teams' : 'Participants'
+  end
+
   def challenge_rank_medal(medal)
     tag.span style: 'font-size:25px;' do
       medal
