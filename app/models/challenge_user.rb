cdcc37862960b79f1352a155842548338c483499
Mohamad <mrahmoun93@gmail.com>
2024-05-04 17:32:26 +0200

    tournament logic

--- app/models/challenge_user.rb
+++ app/models/challenge_user.rb
@@ -21,7 +21,9 @@ class ChallengeUser < ApplicationRecord
   end
 
   def update_score
-    score = self.user.activities.where(date: self.challenge.starts_at.to_date..Date.current).sum(:steps)
+    score = self.user.activities
+                .where(date: self.challenge.starts_at.in_time_zone('Europe/Berlin').to_date..Date.current)
+                .sum(:steps)
 
     self.update(score: score)
   end
@@ -33,7 +35,7 @@ class ChallengeUser < ApplicationRecord
       points = score / 1000
     end
 
-    challenge.tournament.tournament_participants.find_by(user: user).update(score: points)
+    challenge.tournament.tournament_participants.find_by(user: user).increment(:score, points)
   end
 
   def code_validity
--- app/models/tournament_participant.rb
+++ app/models/tournament_participant.rb
@@ -3,10 +3,17 @@ class TournamentParticipant < ApplicationRecord
   belongs_to :user
 
   after_create_commit :create_challenge_users
+  after_destroy_commit :destroy_challenge_users
 
   def create_challenge_users
     tournament.challenges.each do |challenge|
       challenge.challenge_users.create(user: user)
+    end
+  end
+
+  def destroy_challenge_users
+    tournament.challenges.each do |challenge|
+      challenge.challenge_users.find_by(user: user)&.destroy
     end
   end
 end
