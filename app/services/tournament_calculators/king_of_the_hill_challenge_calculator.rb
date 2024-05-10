3dc6ca363709fdc2230e0391674ce856a573c914
Mohamad <mrahmoun93@gmail.com>
2024-05-10 14:36:36 +0200

    relax king of the hill rules

--- app/services/tournament_calculators/king_of_the_hill_challenge_calculator.rb
+++ app/services/tournament_calculators/king_of_the_hill_challenge_calculator.rb
@@ -1,14 +1,14 @@
 module TournamentCalculators
   class KingOfTheHillChallengeCalculator < TournamentChallengeCalculator
     def update_tournament_score
-      if @challenge_user.score >= 7500 && !@challenge.king_appointed?
+      if @challenge_user.score >= 2500 && !@challenge.king_appointed?
         points = 25
 
         @challenge.update(king_id: @challenge_user.tournament_participant_id,
                           king_score: @challenge_user.score, king_at: Time.current)
 
         @challenge_user.update(tournament_score: points)
-      elsif @challenge_user.score >= 7500 && @challenge.king_id != @challenge_user.tournament_participant_id
+      elsif @challenge_user.score >= 2500 && @challenge.king_id != @challenge_user.tournament_participant_id
         remaining_score = @challenge_user.score - @challenge.king_score
 
         return if remaining_score < 500
