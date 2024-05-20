48d1e9aec409f123ef1392e01e7a72360a6fd3e7
Mohamad <mrahmoun93@gmail.com>
2024-05-20 23:57:18 +0200

    do not send challenge reminder if the challenge is already finished

--- app/jobs/challenge_reminder_job.rb
+++ app/jobs/challenge_reminder_job.rb
@@ -1,5 +1,7 @@
 class ChallengeReminderJob < ApplicationJob
   def perform(challenge)
+    return if challenge.ended?
+
     challenge.notify_participants(title: "#{challenge.title} is ending soon!",
                                   body: 'Just 5 hours left! Keep moving to complete your challenge. You got this! 💪')
   end
