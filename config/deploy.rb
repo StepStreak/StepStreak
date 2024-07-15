acbdaf0a3f4638ca43eab7917b8c0889620d40d2
Mohamad <mrahmoun93@gmail.com>
2024-07-16 00:09:25 +0200

    update the used capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.18.0'
+lock '~> 3.19.1'
 
 set :application, 'StepStreak'
 set :repo_url, 'git@github.com:StepStreak/StepStreak.git'
