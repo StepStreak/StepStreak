fdd35ebe34ac3510eefad2f2b1fa554b47cb4938
Mohamad <mrahmoun93@gmail.com>
2024-09-21 01:35:06 +0200

    reduce the amount of information in dashboards

--- app/queries/steps_accumulated_sum_query.rb
+++
@@ -1,8 +0,0 @@
-class StepsAccumulatedSumQuery
-  def self.call
-    result = TotalStepsPerMonth.call
-
-    cumulative_sum = 0
-    result.transform_values! { |v| cumulative_sum += v }
-  end
-end
--- app/queries/total_steps_per_month_year.rb
+++
@@ -1,5 +0,0 @@
-class TotalStepsPerMonthYear
-  def self.call
-    Current.user.activities.group_by_month_of_year(:date).sum(:steps)
-  end
-end
