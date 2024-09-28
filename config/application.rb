44eb11424210d5de4ae6d1334feab342b4275f30
Mohamad <mrahmoun93@gmail.com>
2024-09-28 16:06:07 +0200

    fix ipinfo loading

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'ipinfo' unless defined?(IPinfo)
 
 module StepStreak
   class Application < Rails::Application
